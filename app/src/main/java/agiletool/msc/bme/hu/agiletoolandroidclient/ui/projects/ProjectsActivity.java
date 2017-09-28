cb5bef3c4442d3b6948df251c2d7bda9572d4973
Kazinczi Márton <Marton.Kazinczi@ericsson.com>
2017-09-28 15:16:17 +0200

    start with login

--- app/src/main/java/agiletool/msc/bme/hu/agiletoolandroidclient/ui/projects/ProjectsActivity.java
+++ app/src/main/java/agiletool/msc/bme/hu/agiletoolandroidclient/ui/projects/ProjectsActivity.java
@@ -1,39 +1,16 @@
 package agiletool.msc.bme.hu.agiletoolandroidclient.ui.projects;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import agiletool.msc.bme.hu.agiletoolandroidclient.R;
-import butterknife.ButterKnife;
 
 public class ProjectsActivity extends AppCompatActivity {
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_projects);
-//
-//        ButterKnife.bind(this);
-//    }
-//
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//    }
-//
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
